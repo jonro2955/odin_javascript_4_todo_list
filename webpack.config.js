e81c0ea6fff1a6d9c5a6721d20edd706b47c842b
Jonathan Ro <jonro.2955@gmail.com>
2021-07-04 10:53:48 -0700

    Added viewport settings

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,11 @@ const path = require('path');
 
 module.exports = {
   //mode: change this to 'production' for minified code
-  mode: 'development', 
-  // make webpack use the built in sourcemap feature to locate source errors 
-  devtool: 'inline-source-map',
+  mode: 'production', 
+  /* "devtool: 'inline-source-map'" is a sourcemap feature to 
+  locate source errors. Only use during development since it 
+  dramatically increases bundle size: */
+  // devtool: 'inline-source-map',
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
